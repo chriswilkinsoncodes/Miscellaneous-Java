24a4d9ad2bbc6a9953b1db3fab60c08704456ecb
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2020-11-27 03:29:58 -0500

    AmericanFlag.java

---
+++ AmericanFlag.java
@@ -0,0 +1,16 @@
+//based on:
+//https://github.com/sangnguyen2803/Java-Object-oriented-Programming/blob/
+//main/01_HW_Exercise_Java/src/Homework_Java0101/HW_Exercise01_JavaCore.java
+
+public class AmericanFlag {
+	public static void main(String[] args) {
+		for (int i = 0; i < 4; i++){
+			System.out.println("* * * * * * ==================================");
+			System.out.println(" * * * * *  ==================================");
+		}
+		System.out.println("* * * * * * ==================================");
+		for (int i = 0; i < 6; i++)
+			System.out.println("==============================================");
+	}
+
+}
